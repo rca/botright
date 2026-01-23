3c7376a2d16fa25158cacb911f366f93a05f5b3a
Roberto Aguilar <r@rreboto.com>
2026-01-22 23:44:13 -0500

    Refactor out browser flags
    Make the flags usable outside the method they were defined in.  This allows the caller to be able to customize the flags if needed.

--- botright/__init__.py
+++ botright/__init__.py
@@ -1,7 +1,7 @@
-from .botright import Botright
+from .botright import DEFAULT_FLAGS, Botright
 from .modules.faker import Faker
 from .modules.proxy_manager import ProxyManager
 
 VERSION = "0.5.1"
 
-__all__ = ["Botright", "Faker", "ProxyManager", "VERSION"]
+__all__ = ["DEFAULT_FLAGS", "Botright", "Faker", "ProxyManager", "VERSION"]
